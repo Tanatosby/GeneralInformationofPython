aa49163eeb5423645c9da7bc56845b04e75dba46
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-08 11:27:52 -0500

    Enhance file parsing to include element count and sum
    Added functionality to count elements and calculate their sum.

--- parsetext/parse.py
+++ parsetext/parse.py
@@ -1,12 +1,12 @@
 import re
 
-f = open(input('File: '))
 try:
-
+    f = open(input('File: '))
     str = f.read()
     y = re.findall('[0-9]+',str)
-    sum(y)
-
+    y = [int(i) for i in y ]
+    print('there are',len(y),'elements and the sum is:',sum(y))
+    
 
 
 except:
