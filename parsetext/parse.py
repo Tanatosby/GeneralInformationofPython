2f5b3656d549b051d2b4aa705aea42922e0fbdb7
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-08 11:42:46 -0500

    Refactor parse.py for improved readability

--- parsetext/parse.py
+++ parsetext/parse.py
@@ -6,9 +6,10 @@ try:
     y = re.findall('[0-9]+',str)
     y = [int(i) for i in y ]
     print('there are',len(y),'elements and the sum is:',sum(y))
-    
+    #print('there are',len([int(i) for i in re.findall('[0-9]+',f.read())]),'elements and the sum is:',sum([int(i) for i in re.findall('[0-9]+',f.read())]))
 
 
 except:
     print('error, this is not a file text')
     quit()
+
