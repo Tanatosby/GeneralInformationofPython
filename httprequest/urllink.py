046d7b21f03375a4f0fbc47a6685b192b8cd7f72
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-09 18:05:53 -0500

    Restore and update HTML scraping script

--- httprequest/urllink.py
+++ httprequest/urllink.py
@@ -3,6 +3,8 @@
 Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
 
 We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+web page= https://py4e-data.dr-chuck.net/comments_42.html
+
 '''
 
 from urllib.request import urlopen
@@ -39,5 +41,6 @@ for tag in tags:
     print('Contents:', tag.contents[0])
     print('Attrs:', tag.attrs)
 '''
+
 
 
