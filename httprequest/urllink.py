3396690d40fbf86b856bb9e66431b538dc85e933
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-09 18:03:25 -0500

    Refactor urllink.py for assignment and clarity
    Added assignment instructions and restructured code for clarity.

--- httprequest/urllink.py
+++ httprequest/urllink.py
@@ -1,3 +1,10 @@
+
+'''
+Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
+
+We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+'''
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
@@ -32,4 +39,5 @@ for tag in tags:
     print('Contents:', tag.contents[0])
     print('Attrs:', tag.attrs)
 '''
+
 
