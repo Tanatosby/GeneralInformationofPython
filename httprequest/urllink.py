f9ec77c0036f6908caf4488d79ff97030f73fa2f
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-09 18:02:51 -0500

    Add files via upload

---
+++ httprequest/urllink.py
@@ -0,0 +1,35 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tds = soup('td')
+#print(tags)
+#print(dir(tds[0]))
+n = []
+for td in tds:
+    if len(td.find_all('span'))<1: 
+        continue 
+    for span in td.find_all('span'):
+       n.append(int(span.contents[0]))
+print(sum(n))
+    
+'''
+tags = soup('a')
+for tag in tags:
+    # Look at the parts of a tag
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
+'''
+
