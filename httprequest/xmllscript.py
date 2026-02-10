529fa51151586853dd84badf11dab500ac7b3251
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-09 20:39:14 -0500

    Update xmllscript.py with assignment details
    Added assignment instructions and sample data references.

--- httprequest/xmllscript.py
+++ httprequest/xmllscript.py
@@ -1,3 +1,14 @@
+'''
+
+In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/xml3.py. The program will prompt for a URL, read the XML data from that URL using urllib and then parse and extract the comment counts from the XML data, compute the sum of the numbers in the file.
+
+We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+
+Sample data: http://py4e-data.dr-chuck.net/comments_42.xml (Sum=2553)
+Actual data: http://py4e-data.dr-chuck.net/comments_2370289.xml (Sum ends with 17)
+'''
+
+
 import urllib.request
 import xml.etree.ElementTree as ET
 
@@ -22,3 +33,4 @@ print(nums)
 
 print('Count:', len(nums))
 print('Sum:', sum(nums))
+
