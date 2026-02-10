202ec9e484f5aace2bab526daf2c057a670abbda
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-09 23:41:54 -0500

    Add instructions and update JSON data extraction
    Updated the script to include instructions for extracting data from JSON and provided sample and actual data URLs.

--- httprequest/jsonscript.py
+++ httprequest/jsonscript.py
@@ -1,3 +1,16 @@
+'''
+
+Extracting Data from JSON
+
+In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/json2.py. The program will prompt for a URL, read the JSON data from that URL using urllib and then parse and extract the comment counts from the JSON data, compute the sum of the numbers in the file and enter the sum below:
+
+We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+
+Sample data: http://py4e-data.dr-chuck.net/comments_42.json (Sum=2553)
+Actual data: http://py4e-data.dr-chuck.net/comments_2370290.json (Sum ends with 85)
+
+'''
+
 import json
 
 import urllib.request
@@ -37,4 +50,5 @@ for item in info:
     print('Name', item['name'])
     print('Id', item['id'])
     print('Attribute', item['x'])
+
 '''
