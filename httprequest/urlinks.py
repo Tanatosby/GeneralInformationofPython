9538066d5ca7be993c44fcdf92035d7dd6449007
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-09 19:04:25 -0500

    Add assignment details and enhance URL processing
    Updated the script to include assignment details and instructions for processing URLs.

--- httprequest/urlinks.py
+++ httprequest/urlinks.py
@@ -3,7 +3,20 @@
 # or pip install beautifulsoup4 to ensure you have the latest version
 # and unzip it in the same directory as this file
 #deprecated, for now installing with pip 
+'''
+In this assignment you will write a Python program that expands on http://www.py4e.com/code3/urllinks.py. The program will use urllib to read the HTML from the data files below, extract the href= vaues from the anchor tags, scan for a tag that is in a particular position relative to the first name in the list, follow that link and repeat the process a number of times and report the last name you find.
 
+We provide two files for this assignment. One is a sample file where we give you the name for your testing and the other is the actual data you need to process for the assignment
+
+Sample problem: Start at http://py4e-data.dr-chuck.net/known_by_Fikret.html
+Find the link at position 3 (the first name is 1). Follow that link. Repeat this process 4 times. The answer is the last name that you retrieve.
+Sequence of names: Fikret Montgomery Mhairade Butchi Anayah
+Last name in sequence: Anayah
+Actual problem: Start at: http://py4e-data.dr-chuck.net/known_by_Jerry.html
+Find the link at position 18 (the first name is 1). Follow that link. Repeat this process 7 times. The answer is the last name that you retrieve.
+Hint: The first character of the name of the last page that you will load is: A
+
+'''
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl # defaults to certificate verification and most secure protocol (now TLS)
@@ -30,4 +43,5 @@ while i<4:
 
     url = l[2]
     print(re.search('_(.+).html',url))
+
     i+=1
